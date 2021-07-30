37d6244a129021509b986da578c5c16c6125789b
Ku6epnec <ku6epnec@rambler.ru>
2021-07-30 18:12:01 +0300

    Strategy_v.0.05

---
+++ Assets/Scripts/UserControlSystem/UI/Presenter/MouseInteractionsPresenter.cs
@@ -0,0 +1,30 @@
+using System.Linq;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MouseInteractionsPresenter: MonoBehaviour
+{
+    [SerializeField] private Camera _camera;
+    [SerializeField] private SelectableValue _selectedObject;
+    [SerializeField] private EventSystem _eventSystem;
+
+    private GameObject SelectedObject;
+
+    private void Update()
+    {
+        if (!Input.GetMouseButtonUp(0) || _eventSystem.IsPointerOverGameObject())
+        {
+            return;
+        }
+        var hits = Physics.RaycastAll(_camera.ScreenPointToRay(Input.mousePosition));
+        if (hits.Length == 0)
+        {
+            return;
+        }
+        var selectable = hits
+                .Select(hit => hit.collider.GetComponentInParent<ISelecatable>())
+                .Where(c => c != null)
+                .FirstOrDefault();
+        _selectedObject.SetValue(selectable);
+    }
+}
